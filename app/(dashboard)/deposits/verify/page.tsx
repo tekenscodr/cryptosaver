c3f779359235722e7310ff9df156bf873cf26185
tekenscodr <ensleybdarku@gmail.com>
2024-10-01 04:12:31 +0100

    suspense added

--- app/(dashboard)/deposits/verify/page.tsx
+++ app/(dashboard)/deposits/verify/page.tsx
@@ -2,28 +2,31 @@
 import { Card, CardContent } from '@/components/ui/card'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const Page = () => {
     const searchParams = useSearchParams();
     const reference = searchParams.get('reference');
 
     return (
-        <div className='flex justify-center items-center'>
-            <Card className='flex justify-center items-center'>
-                <CardContent>
-                    {reference && (
-                        <p>Payment Verified with reference: {reference}</p>
-                    )}
-                    {!reference && (
-                        <p>Payment Verified</p>
-                    )}
-                    <Link href={'/deposts'}>
-                        Back to transactions
-                    </Link>
-                </CardContent>
-            </Card>
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+
+            <div className='flex justify-center items-center'>
+                <Card className='flex justify-center items-center'>
+                    <CardContent>
+                        {reference && (
+                            <p>Payment Verified with reference: {reference}</p>
+                        )}
+                        {!reference && (
+                            <p>Payment Verified</p>
+                        )}
+                        <Link href={'/deposts'}>
+                            Back to transactions
+                        </Link>
+                    </CardContent>
+                </Card>
+            </div>
+        </Suspense>
     )
 }
 
