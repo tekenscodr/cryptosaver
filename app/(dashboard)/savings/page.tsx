1dd60aed2e65636c51209dd77fb93870d36d6d52
tekenscodr <ensleybdarku@gmail.com>
2024-10-01 09:12:40 +0100

    goals

--- app/(dashboard)/savings/page.tsx
+++ app/(dashboard)/savings/page.tsx
@@ -65,7 +65,7 @@ const Savings = () => {
                 </CardHeader>
                 <CardContent>
 
-                    <SavingsPlans data={goal} />
+                    <SavingsPlans data={goals} />
                 </CardContent>
             </Card>
         </div>
