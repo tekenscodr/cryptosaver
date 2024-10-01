dfaa6c7aa25cd95df6a4d59855c4b10ccb30ff2b
tekenscodr <ensleybdarku@gmail.com>
2024-10-01 11:07:03 +0100

    float

--- app/api/deposit/contribute/route.ts
+++ app/api/deposit/contribute/route.ts
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import prisma from '../../../prismadb'
 import { NextResponse } from 'next/server';
+import { Decimal } from '@prisma/client/runtime/library';
 
 export async function PUT(request: Request) {
     const { getUser } = getKindeServerSession();
@@ -24,12 +25,14 @@ export async function PUT(request: Request) {
                 account_balance: true
             }
         })
+        const decimal = await (Number(getFiat?.account_balance) - Number(body.amount)).toFixed(2)
         const deduction = await prisma.fiatAc.update({
             where: {
                 userId: user?.id
             },
             data: {
-                account_balance: Number(getFiat?.account_balance) - Number(body.amount)
+                account_balance: parseFloat((Number(getFiat?.account_balance) - Number(body.amount)).toFixed(2))
+
             }
         })
         return NextResponse.json({ deduction })
